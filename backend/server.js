d9fe1e68e01f21e8c7ac1834178ec08e06d9362f
Windyphan <thandongdatviettatca@gmail.com>
2025-12-31 19:34:33 +0000

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,8 @@ app.use(helmet());
 const allowedOrigins = [
   'http://localhost:3000',
   'https://localhost:3000',
-  'https://www.phongphan.me',
-  'https://phongphan.me',
+    'https://www.trangthanhtravel.com',
+    'https://trangthanhtravel.com',
   process.env.FRONTEND_URL
 ].filter(Boolean); // Remove any undefined values
 
